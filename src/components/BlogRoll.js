e0635869d81bdcad0043bcc4793df87da02db2ff
Den <anton.tim4enko.2014@gmail.com>
2019-05-21 11:27:33 +0300

    update config for multi language support

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,72 +1,105 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql, StaticQuery } from "gatsby";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
 class BlogRoll extends React.Component {
+  state = {
+    language: "ru"
+  };
+  onCheckboxChange = e => {
+    this.setState({
+      language: e.currentTarget.value
+    });
+  };
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${
-                            post.title
-                          }`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
+      <>
+        <div className="column">
+          <h2>Language select</h2>
+          <label className='is-block label'>
+            English
+          <input
+            type="radio"
+            name="language"
+            value="en"
+            checked={this.state.language === "en"}
+            onChange={this.onCheckboxChange}
+          />
+          </label>
+          <label className='is-block label'>
+            Russian
+          <input
+            type="radio"
+            name="language"
+            value="ru"
+            checked={this.state.language === "ru"}
+            onChange={this.onCheckboxChange}
+          />
+          </label>
+        </div>
+        <div className="columns is-multiline">
+          {posts &&
+            posts.map(({ node: post }) => (
+              <div className="is-parent column is-6" key={post.id}>
+                <article
+                  className={`blog-list-item tile is-child box notification ${
+                    post.frontmatter.featuredpost ? "is-featured" : ""
+                  }`}
+                >
+                  <header>
+                    {post.frontmatter.featuredimage ? (
+                      <div className="featured-thumbnail">
+                        <PreviewCompatibleImage
+                          imageInfo={{
+                            image: post.frontmatter.featuredimage,
+                            alt: `featured image thumbnail for post ${
+                              post.title
+                            }`
+                          }}
+                        />
+                      </div>
+                    ) : null}
+                    <p className="post-meta">
+                      <Link
+                        className="title has-text-primary is-size-4"
+                        to={post.fields.slug}
+                      >
+                        {post.frontmatter.title}
+                      </Link>
+                      <span> &bull; </span>
+                      <span className="subtitle is-size-5 is-block">
+                        {post.frontmatter.date}
+                      </span>
+                    </p>
+                  </header>
+                  <p>
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <Link className="button" to={post.fields.slug}>
+                      Keep Reading →
                     </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
                   </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
-          ))}
-      </div>
-    )
+                </article>
+              </div>
+            ))}
+        </div>
+      </>
+    );
   }
 }
 
 BlogRoll.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+      edges: PropTypes.array
+    })
+  })
+};
 
 export default () => (
   <StaticQuery
@@ -103,4 +136,4 @@ export default () => (
     `}
     render={(data, count) => <BlogRoll data={data} count={count} />}
   />
-)
+);
